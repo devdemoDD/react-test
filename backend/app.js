b744c44d0148a0aaac852eccea56309322d3a901
jjanmo <jjanmo07@hanmail.net>
2023-01-30 00:31:38 +0900

    feat: configure cors in backend

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -25,7 +26,7 @@ const options = {
 const openapiSpec = swaggerJSDoc(options);
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(openapiSpec));
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
